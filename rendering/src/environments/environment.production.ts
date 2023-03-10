e4a6393e87f921cff8a43cff120878f9351442e1
Andreas Haller <andreas.haller@netcentric.biz>
2023-03-10 08:36:59 +0100

    fix publisher url

--- rendering/src/environments/environment.production.ts
+++ rendering/src/environments/environment.production.ts
@@ -4,8 +4,8 @@ export const environment = {
   ...defaults,
   production: true,
   apis: {
-    structure: 'https://publish-p97903-e899214.adobeaemcloud.com/apps/rockstar-2023/services/structure.public.json{path}',
-    content: 'https://publish-p97903-e899214.adobeaemcloud.com/apps/rockstar-2023/services/content.public.html{path}',
-    teaserList: 'https://publish-p97903-e899214.adobeaemcloud.com/apps/rockstar-2023/services/teaser-list.public.json{path}',
+    structure: 'https://publish-p97903-e990869.adobeaemcloud.com/apps/rockstar-2023/services/structure.public.json{path}',
+    content: 'https://publish-p97903-e990869.adobeaemcloud.com/apps/rockstar-2023/services/content.public.html{path}',
+    teaserList: 'https://publish-p97903-e990869.adobeaemcloud.com/apps/rockstar-2023/services/teaser-list.public.json{path}',
   },
 };
